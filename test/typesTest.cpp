6a9495641367f7d9377ed35070ed5a1f4a059709
grkon <yo.for.user@gmail.com>
2023-05-30 17:09:23 +0900

    add MakeFenceTest

--- test/typesTest.cpp
+++ test/typesTest.cpp
@@ -24,14 +24,14 @@ TEST(TypesTest, MakeSquareTest)
 
     // successfully make
 
-    se1 = SE_A8; // 0x1010000
-    se2 = SE_B9; // 0x1011011
+    se1 = SE_A8; // 0b1010000
+    se2 = SE_B9; // 0b1011011
 
     EXPECT_EQ(0x5b50, MakeSquare(se1, se2));
     EXPECT_EQ(0x5b50, MakeSquare(se2, se1));
 
-    se1 = SE_D4; // 0x101011 (+1)
-    se2 = SE_E3; // 0x100010 (-1)
+    se1 = SE_D4; // 0b101011 (+1)
+    se2 = SE_E3; // 0b100010 (-1)
 
     EXPECT_EQ(0x2c21, MakeSquare(se1, se2));
     EXPECT_EQ(0x2c21, MakeSquare(se2, se1));
@@ -49,4 +49,31 @@ TEST(TypesTest, MakeSquareTest)
 
     EXPECT_EQ(Square::SquareInvalid, MakeSquare(se1, se2));
     EXPECT_EQ(Square::SquareInvalid, MakeSquare(se2, se1));
+}
+
+TEST(TypesTest, MakeFenceTest)
+{
+    SquareEdge se1, se2;
+
+    // successfully make
+
+    se1 = SE_D3; // 0b100001
+    se2 = SE_D5; // 0b110101
+
+    EXPECT_EQ(0x3521, MakeFence(se1, se2));
+    EXPECT_EQ(0x3521, MakeFence(se2, se1));
+
+    se1 = SE_F7; // 0b1001011
+    se2 = SE_H7; // 0b1001101
+
+    EXPECT_EQ(0x4d4b, MakeFence(se1, se2));
+    EXPECT_EQ(0x4d4b, MakeFence(se2, se1));
+
+    // fatal
+
+    se1 = SE_A7;
+    se2 = SE_I6;
+
+    EXPECT_EQ(Fence::FenceInvalid, MakeFence(se1, se2));
+    EXPECT_EQ(Fence::FenceInvalid, MakeFence(se2, se1));
 }
