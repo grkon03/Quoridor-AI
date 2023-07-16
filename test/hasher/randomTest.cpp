5152d46e6b8b05b64231e7de8d0e4b5181a7ef48
grkon <yo.for.user@gmail.com>
2023-07-16 11:20:30 +0900

    add RandomizerTest

--- test/hasher/randomTest.cpp
+++ test/hasher/randomTest.cpp
@@ -1,4 +1,29 @@
 #include <gtest/gtest.h>
 #include <iostream>
+#include <vector>
 #include "../../src/includes/types.hpp"
 #include "../../src/includes/hasher/HasherAllIncludes.hpp"
+
+using namespace QuoridorAI;
+using namespace QuoridorAI::Hasher;
+
+// verify no-conflict of MTRandomizer
+TEST(RandomizerTest, MTRandomizerTest)
+{
+    Randomizer *r = new MTRandomizer(64, 2906);
+    std::vector<HashKey> list;
+    int size = 290;
+
+    for (int i = 0; i < size; ++i)
+    {
+        list.push_back(r->next());
+    }
+
+    for (int i = 0; i < size; ++i)
+    {
+        for (int j = i + 1; j < size; ++j)
+        {
+            EXPECT_NE(list[i], list[j]);
+        }
+    }
+}
