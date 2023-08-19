5970377d60536221c868c86f3f6dcad2567957fb
grkon <yo.for.user@gmail.com>
2023-08-19 15:52:59 +0900

    add WallBBOD Vertical IsThereWall test

--- test/wallTest.cpp
+++ test/wallTest.cpp
@@ -13,7 +13,7 @@ TEST(WallBBODTest, WallBBODHorizontalIsThereWallTest)
     hw = WallBBOD<Horizontal>(Bitboard96(
         "0b100000000001000000000100000000010000000001000000000100000000010000000001"));
 
-    // all the true conditinos
+    // all the true conditions
 
     EXPECT_TRUE(hw.IsThereWall(FileA, Rank1));
     EXPECT_TRUE(hw.IsThereWall(FileB, Rank2));
@@ -31,4 +31,34 @@ TEST(WallBBODTest, WallBBODHorizontalIsThereWallTest)
     EXPECT_FALSE(hw.IsThereWall(FileE, Rank2));
     EXPECT_FALSE(hw.IsThereWall(FileF, Rank3));
     EXPECT_FALSE(hw.IsThereWall(FileH, Rank7));
+}
+
+TEST(WallBBODTest, WalBBODVerticalIsThereWallTest)
+{
+    WallBBOD<Vertical> vw;
+
+    /*
+        wall existing at: b0-b1, c2-c3, d3-d4, e4-e5, f5-f6, g6-g7, h7-h8, i8-i9
+    */
+    vw = WallBBOD<Vertical>(Bitboard96(
+        "0b100000000100000000100000000100000000100000000100000000100000000000000001"));
+
+    // all the true conditions
+
+    EXPECT_TRUE(vw.IsThereWall(FileB, Rank0));
+    EXPECT_TRUE(vw.IsThereWall(FileC, Rank2));
+    EXPECT_TRUE(vw.IsThereWall(FileD, Rank3));
+    EXPECT_TRUE(vw.IsThereWall(FileE, Rank4));
+    EXPECT_TRUE(vw.IsThereWall(FileF, Rank5));
+    EXPECT_TRUE(vw.IsThereWall(FileG, Rank6));
+    EXPECT_TRUE(vw.IsThereWall(FileH, Rank7));
+    EXPECT_TRUE(vw.IsThereWall(FileI, Rank8));
+
+    // some false conditions
+
+    EXPECT_FALSE(vw.IsThereWall(FileB, Rank1));
+    EXPECT_FALSE(vw.IsThereWall(FileD, Rank8));
+    EXPECT_FALSE(vw.IsThereWall(FileE, Rank3));
+    EXPECT_FALSE(vw.IsThereWall(FileF, Rank6));
+    EXPECT_FALSE(vw.IsThereWall(FileH, Rank5));
 }
