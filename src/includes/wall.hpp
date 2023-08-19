22bccc64d86cd25f273f1633c00e0369a279eba6
grkon <yo.for.user@gmail.com>
2023-08-19 14:55:55 +0900

    fix a few

--- src/includes/wall.hpp
+++ src/includes/wall.hpp
@@ -25,13 +25,13 @@ namespace QuoridorAI
         /**
          * @brief verify whether there is wall at one section of walls
          *
-         * @param rank lower rank of square edges of the section
          * @param file lower file of square edges of the section
+         * @param rank lower rank of square edges of the section
          *
          * @note should Bottom-Left Square edge
          * @note for speed, no verification that rank or file is out of range
          */
-        bool IsThereWall(Rank rank, File file) const;
+        bool IsThereWall(File file, Rank rank) const;
 
         /**
          * @brief verify whether there is wall at one section of walls
@@ -52,14 +52,14 @@ namespace QuoridorAI
     WallBBOD<direction>::WallBBOD(const WallBBOD &wb) : Bitboard96(wb) {}
 
     template <>
-    inline bool WallBBOD<Horizontal>::IsThereWall(Rank rank, File file) const
+    inline bool WallBBOD<Horizontal>::IsThereWall(File file, Rank rank) const
     {
-        return (operator&(Constant::oneBitMask96[(rank - 1) * 9 + file]) == 0);
+        return (operator&(Constant::oneBitMask96[(rank - 1) * 9 + file]) != 0);
     }
     template <>
-    inline bool WallBBOD<Vertical>::IsThereWall(Rank rank, File file) const
+    inline bool WallBBOD<Vertical>::IsThereWall(File file, Rank rank) const
     {
-        return (operator&(Constant::oneBitMask96[rank * 9 + file - 1]) == 0);
+        return (operator&(Constant::oneBitMask96[rank * 9 + file - 1]) != 0);
     }
 
     template <WallDir direction>
