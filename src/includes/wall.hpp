9eb15682c4328ba647491443918949eaddbb168b
grkon <yo.for.user@gmail.com>
2023-08-19 16:09:59 +0900

    fix locations of some comments

--- src/includes/wall.hpp
+++ src/includes/wall.hpp
@@ -52,17 +52,44 @@ namespace QuoridorAI
     WallBBOD<direction>::WallBBOD(const WallBBOD &wb) : Bitboard96(wb) {}
 
     template <>
+    /**
+     * @brief verify whether there is wall at one section of walls
+     *
+     * @param file lower file of square edges of the section
+     * @param rank rank of the section of walls
+     *
+     * @note should Bottom-Left Square edge
+     * @note for speed, no verification that rank or file is out of range
+     */
     inline bool WallBBOD<Horizontal>::IsThereWall(File file, Rank rank) const
     {
         return (operator&(Constant::oneBitMask96[(rank - 1) * 9 + file]) != 0);
     }
+
     template <>
+    /**
+     * @brief verify whether there is wall at one section of walls
+     *
+     * @param file file of the section of walls
+     * @param rank lower rank of square edges of the section
+     *
+     * @note should Bottom-Left Square edge
+     * @note for speed, no verification that rank or file is out of range
+     */
     inline bool WallBBOD<Vertical>::IsThereWall(File file, Rank rank) const
     {
         return (operator&(Constant::oneBitMask96[rank * 8 + file - 1]) != 0);
     }
 
     template <WallDir direction>
+    /**
+     * @brief verify whether there is wall at one section of walls
+     *
+     * @param se bottom-left one of square edges of the section
+     *
+     * @note should Bottom-Left Square edge
+     * @note for speed, no verification that square edge
+     */
     inline bool WallBBOD<direction>::IsThereWall(SquareEdge se) const
     {
         return IsThereWall(GetRank(se), GetFile(se));
