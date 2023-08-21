dc35e0f39ec18e547efd88ec88a1f9eaac4f2135
grkon <yo.for.user@gmail.com>
2023-08-22 02:33:07 +0900

    make WallMan class definition

---
+++ src/includes/wallman.hpp
@@ -0,0 +1,75 @@
+#pragma once
+
+#include "wall.hpp"
+
+namespace QuoridorAI
+{
+    /**
+     * @brief Wall Manager
+     *
+     */
+    class WallMan
+    {
+        // variables
+
+        /**
+         * @brief main wall bitboard
+         *
+         */
+        WallBBs wallBBs;
+
+        /**
+         * @brief bitboard for across fence such that:
+         * C3C5 and B4D4, in this case, these fences have intersection at C4.
+         * Bits is set which the center square edge of fences
+         *
+         */
+        Bitboard96 acrossBB;
+
+    public:
+        // constructors
+
+        WallMan();
+        /**
+         * @note not recommended
+         *
+         */
+        WallMan(WallBBs);
+        WallMan(WallBBs, Bitboard96);
+        WallMan(const WallMan &);
+
+        // functions
+
+        /**
+         * @brief Get the WallBBs object
+         *
+         * @return WallBBs
+         */
+        WallBBs getWallBBs() const;
+
+        template <WallDir direction>
+        WallBBOD<direction> getWallBBOD() const;
+
+        /**
+         * @brief put fence
+         *
+         */
+        void PutFence(Fence);
+
+        /**
+         * @brief put fence
+         *
+         * @tparam direction the fence direction
+         * @param se left-bottom square edge of the fence
+         */
+        template <WallDir direction>
+        void PutFence(SquareEdge se);
+
+        /**
+         * @brief put fence
+         *
+         * @param fenceIndex index of the fence
+         */
+        void PutFence(int fenceIndex);
+    };
+}
