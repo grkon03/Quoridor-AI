02634511df6aaae14867e6c178f844dfd1433103
grkon <yo.for.user@gmail.com>
2023-11-16 14:31:31 +0900

    setup boardtest

--- test/boardTest.cpp
+++ test/boardTest.cpp
@@ -4,6 +4,38 @@
 
 using namespace QuoridorAI;
 
+/*
+    BoardInfo memo:
+
+    //central
+
+    WallMan wallMan;
+    int kingSquareIndex[ColorLimit];
+    unsigned char numberOfRemainingFence[ColorLimit]; // -1 expresses no square
+    Color turnPlayer;                                 // player to do move in this turn
+    int kingMovableSquaresIndex[ColorLimit][5];
+
+    Bitboard128 availableFenceBB;
+    Bitboard128 usedSquareEdgeBB;
+
+    int turnSpent; // start with 0
+
+    // hash
+
+    Hasher::ZobristHash hash;
+
+    // recorder
+
+    std::vector<int> moveRecorder[ColorLimit];
+    std::vector<int> kingRecorder[ColorLimit];
+*/
+
 TEST(BoardTest, BoardDoMoveTest)
 {
+    BoardInfo biStart, biCor;
+    WallMan wmStart, wmCor;
+
+    biStart = BoardInfo{
+
+    };
 }
